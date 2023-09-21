8a3df4c23600ebae88e280e11f77a88c3da05eca
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-09-22 00:12:21 +0300

    add: prisma client setup and getWords query

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,9 +2,15 @@
 
 import { useTranslations } from "next-intl";
 import Link from "next-intl/link";
+import { trpc } from "../_trpc/client";
 
 export default function Index() {
   const t = useTranslations("Index");
+  const words = trpc.getWords.useQuery();
+  if (!words.data) {
+    console.log("loading");
+  }
+  console.log(words.data);
   return (
     <div>
       <h1>{t("title")}</h1>
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,11 @@
+import prisma from "@/db";
 import { router, publicProcedure } from "./trpc";
-
 export const appRouter = router({
   helloWorld: publicProcedure.query(() => {
     return "Hello World!";
+  }),
+  getWords: publicProcedure.query(async () => {
+    return await prisma.word.findMany({ include: { meanings: true } });
   }),
 });
 
