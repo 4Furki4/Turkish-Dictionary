9a9bec65dad1660526fd46ae5a98503dfb410238
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-09-22 08:52:47 +0300

    add: getWord by name and procedure documentations

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,9 @@
 "use client";
-
 import { useTranslations } from "next-intl";
 import Link from "next-intl/link";
-import { trpc } from "../_trpc/client";
 
 export default function Index() {
   const t = useTranslations("Index");
-  const words = trpc.getWords.useQuery();
-  if (!words.data) {
-    console.log("loading");
-  }
-  console.log(words.data);
   return (
     <div>
       <h1>{t("title")}</h1>
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,48 @@
 import prisma from "@/db";
+import { z } from "zod";
 import { router, publicProcedure } from "./trpc";
 export const appRouter = router({
   helloWorld: publicProcedure.query(() => {
     return "Hello World!";
   }),
-  getWords: publicProcedure.query(async () => {
-    return await prisma.word.findMany({ include: { meanings: true } });
-  }),
+  /**
+   * Get all words from database with pagination
+   */
+  getWords: publicProcedure
+    .input(
+      z.object({
+        take: z.number().optional().default(5),
+        skip: z.number().optional().default(0),
+      })
+    )
+    .query(async ({ input }) => {
+      return await prisma.word.findMany({
+        take: input.take,
+        skip: input.skip,
+        include: { meanings: true },
+      });
+    }),
+  /**
+   * Get a word by name quering the database
+   */
+  getWord: publicProcedure
+    .input(
+      z.string({
+        invalid_type_error: "Word must be a string",
+        required_error: "Word is required to get a word",
+      })
+    )
+    .query(async ({ input }) => {
+      const words = await prisma.word.findMany({
+        where: {
+          name: input,
+        },
+        include: {
+          meanings: true,
+        },
+      });
+      return words || "Not found any word";
+    }),
 });
 
 export type AppRouter = typeof appRouter;
