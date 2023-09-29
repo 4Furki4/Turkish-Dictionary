101f5a19ccb8ddd332e5052237db4a290c2c5cd7
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-09-29 16:40:33 +0300

    fix: signup page title and description generated on the server side

---
+++ src/components/customs/Signup.tsx
@@ -0,0 +1,294 @@
+"use client";
+
+import { onEnterAndSpace } from "@/lib/keyEvents";
+import { Button, Divider, Input } from "@nextui-org/react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next-intl/client";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
+type SignUpInputs = {
+  name: string;
+  username: string;
+  email: string;
+  signupPassword: string;
+  confirmPassword: string;
+};
+type LoginInputs = {
+  usernameOrEmail: string;
+  loginPassword: string;
+};
+
+export default function Signup() {
+  const onSignupSubmit: SubmitHandler<SignUpInputs> = (data) => {
+    console.log(data);
+  };
+  const onLoginSubmit: SubmitHandler<LoginInputs> = (data) => {
+    console.log(data);
+  };
+  const onProviderSignin = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: decodeURIComponent(params.get("callbackUrl") ?? "/"),
+    });
+  };
+  const {
+    register,
+    handleSubmit,
+    control,
+    watch,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm<SignUpInputs & LoginInputs>({ mode: "all" });
+  const params = useSearchParams();
+  const router = useRouter();
+  const userParam = params.get("user");
+  return (
+    <div className="absolute grid place-items-center w-full h-[calc(100%-64px)] p-2">
+      {userParam && userParam === "new" ? (
+        <form
+          onSubmit={handleSubmit(onSignupSubmit)}
+          className="flex flex-col gap-2 w-11/12 sm:w-full max-w-xl bg-background/70 backdrop-saturate-150 p-6 sm:p-12 rounded-xl"
+        >
+          <Button
+            variant="bordered"
+            onClick={() => onProviderSignin("google")}
+            onKeyDown={(e) =>
+              onEnterAndSpace(e, () => onProviderSignin("google"))
+            }
+            startContent={
+              <Image
+                src={"/svg/providers/google.svg"}
+                width={24}
+                height={24}
+                alt="google-icon"
+              />
+            }
+          >
+            Sign up with Google
+          </Button>
+          <Divider></Divider>
+          <div>
+            <h1 className="text-xl sm:text-2xl lg:text-4xl font-bold">
+              Create a new account
+            </h1>
+          </div>
+          <Controller
+            name="name"
+            rules={{
+              required: true,
+              onChange: (e) => {
+                if (e.target.value.length > 0) {
+                  clearErrors("name");
+                }
+              },
+            }}
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                {...field}
+                label="Name"
+                isRequired
+                color="primary"
+                variant="underlined"
+                errorMessage={errors.name?.message}
+                isInvalid={error !== undefined}
+              />
+            )}
+          />
+          <Controller
+            name="username"
+            rules={{
+              required: true,
+              onChange: (e) => {
+                if (e.target.value.length > 0) {
+                  clearErrors("username");
+                }
+              },
+            }}
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                {...field}
+                label="Username"
+                isRequired
+                color="primary"
+                variant="underlined"
+                errorMessage={errors.username?.message}
+                isInvalid={error !== undefined}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="email"
+            rules={{
+              required: true,
+              pattern: {
+                value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                message: "Please enter a valid email",
+              },
+            }}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                type="email"
+                {...field}
+                label="Email"
+                isRequired
+                color="primary"
+                variant="underlined"
+                errorMessage={errors.email?.message}
+                isInvalid={error !== undefined}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="signupPassword"
+            rules={{
+              required: true,
+              minLength: {
+                value: 8,
+                message: "Password must have at least 8 characters",
+              },
+            }}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                {...field}
+                label="Password"
+                isRequired
+                color="primary"
+                variant="underlined"
+                errorMessage={errors.signupPassword?.message}
+                isInvalid={error !== undefined}
+                type="password"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="confirmPassword"
+            rules={{
+              required: true,
+              validate: (value) =>
+                value === watch("signupPassword") || "Passwords do not match",
+            }}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                {...field}
+                label="Confirm password"
+                isRequired
+                color="primary"
+                variant="underlined"
+                errorMessage={errors.confirmPassword?.message}
+                isInvalid={error !== undefined}
+                type="password"
+              />
+            )}
+          />
+
+          <Button color="primary" variant="ghost" type="submit">
+            Sign up
+          </Button>
+          <p>
+            Are you already signed up ?{" "}
+            <span
+              className="underline cursor-pointer"
+              onClick={() =>
+                router.push(
+                  `${
+                    params.get("callbackUrl")
+                      ? `?callbackUrl=${params.get("callbackUrl")}`
+                      : ""
+                  }`,
+                  { scroll: false }
+                )
+              }
+            >
+              Login
+            </span>
+          </p>
+        </form>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onLoginSubmit)}
+          className="flex flex-col gap-2 w-11/12 sm:w-full max-w-xl bg-background/70 backdrop-saturate-150 p-6 sm:p-12 rounded-xl"
+        >
+          <Button
+            variant="bordered"
+            onClick={() => onProviderSignin("google")}
+            onKeyDown={(e) =>
+              onEnterAndSpace(e, () => onProviderSignin("google"))
+            }
+            startContent={
+              <Image
+                src={"/svg/providers/google.svg"}
+                width={24}
+                height={24}
+                alt="google-icon"
+              />
+            }
+          >
+            Sign in with Google
+          </Button>
+          <Divider></Divider>
+          <Controller
+            name="usernameOrEmail"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                {...field}
+                label="Username or Email"
+                isRequired
+                color="primary"
+                variant="underlined"
+                errorMessage={errors.usernameOrEmail?.message}
+                isInvalid={error !== undefined}
+              />
+            )}
+          />
+          <Controller
+            name="loginPassword"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                {...field}
+                label="Password"
+                isRequired
+                color="primary"
+                variant="underlined"
+                isInvalid={error !== undefined}
+                type="password"
+              />
+            )}
+          />
+          <Button color="primary" variant="ghost" type="submit">
+            Sign In
+          </Button>
+          <p>
+            {"Don't have an account ? "}
+            <span
+              className="underline cursor-pointer"
+              onClick={() =>
+                router.push(
+                  `${
+                    params.get("callbackUrl")
+                      ? `?callbackUrl=${params.get("callbackUrl")}&user=new`
+                      : "?user=new"
+                  }`,
+                  { scroll: false }
+                )
+              }
+            >
+              Sign up
+            </span>
+          </p>
+        </form>
+      )}
+    </div>
+  );
+}
