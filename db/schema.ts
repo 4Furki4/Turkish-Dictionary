c1810b7b18594b646d82114b9596bbb1717eaf97
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-11-24 11:48:19 +0300

    add: renewed words and meanings tables, user and account tables relatedWords tables were removed since it didn't work well...

--- db/schema.ts
+++ db/schema.ts
@@ -3,78 +3,57 @@ import {
   pgEnum,
   pgTable,
   serial,
-  uniqueIndex,
   varchar,
   text,
   date,
+  timestamp,
   primaryKey,
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
+import type { AdapterAccount } from "@auth/core/adapters";
 
 export const words = pgTable("words", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 255 }).notNull(),
   phonetic: varchar("phonetic", { length: 255 }),
   root: varchar("root", { length: 255 }),
-  attributes: varchar("attributes", { length: 255 }),
-  audio: varchar("audio", { length: 255 }),
-  createdAt: date("createdAt").notNull(),
-  updatedAt: date("updatedAt").notNull(),
+  attributes: varchar("attributes", { length: 255 }).array(),
+  audioUrl: varchar("audio", { length: 255 }), // will also have many-to-many relation with words and users in the future
+  createdAt: date("createdAt").defaultNow(),
+  updatedAt: date("updatedAt"),
+  relatedWords: varchar("related_words", { length: 255 }).array(),
+  relatedPhrases: text("related_phrases").array(),
 });
 
 export const wordsRelations = relations(words, ({ many }) => ({
   meanings: many(meanings),
-  wordsToRelatedWords: many(wordsToRelatedWords),
+  usersToWords: many(usersToWords),
 }));
 
-export const relatedWords = pgTable("related_words", {
-  id: serial("id").primaryKey(),
-  relatedWordId: integer("related_word_id")
-    .notNull()
-    .references(() => words.id),
-});
+export const partOfSpeechEnum = pgEnum("partOfSpeech", [
+  "isim",
+  "fiil",
+  "sıfat",
+  "zarf",
+  "zamir",
+  "ünlem",
+  "edat",
+  "bağlaç",
+]);
 
-export const relatedWordsRelations = relations(relatedWords, ({ many }) => ({
-  wordsToRelatedWords: many(wordsToRelatedWords),
-}));
-export const wordsToRelatedWords = pgTable(
-  "words_to_related_words",
-  {
-    wordId: integer("word_id")
-      .notNull()
-      .references(() => words.id),
-    relatedWordId: integer("related_word_id")
-      .notNull()
-      .references(() => relatedWords.id),
-  },
-  (t) => ({
-    pk: primaryKey({ columns: [t.wordId, t.relatedWordId] }),
-  })
-);
-
-export const wordsToRelatedWordsRelations = relations(
-  wordsToRelatedWords,
-  ({ one }) => ({
-    words: one(words, {
-      fields: [wordsToRelatedWords.wordId],
-      references: [words.id],
-    }),
-    relatedWords: one(words, {
-      fields: [wordsToRelatedWords.relatedWordId],
-      references: [words.id],
-    }),
-  })
-);
 export const meanings = pgTable("meanings", {
   id: serial("id").primaryKey(),
   definition: text("definition").notNull(),
-  exampleSentece: text("exampleSentence").notNull(),
-  exampleSentenceAuthor: varchar("exampleAuthor", { length: 255 }).notNull(),
+  exampleSentece: text("exampleSentence"),
+  exampleSentenceAuthor: varchar("exampleAuthor", { length: 255 }),
   wordId: integer("word_id")
     .notNull()
     .references(() => words.id, {
       onDelete: "cascade",
     }),
+  imageUrl: varchar("imageUrl", { length: 255 }),
+  partOfSpeech: partOfSpeechEnum("partOfSpeech").notNull(),
+  attributes: varchar("attributes", { length: 255 }).array(),
 });
 
 export const meaningsRelations = relations(meanings, ({ one }) => ({
@@ -83,3 +62,72 @@ export const meaningsRelations = relations(meanings, ({ one }) => ({
     references: [words.id],
   }),
 }));
+
+export const usersToWords = pgTable(
+  "users_to_groups",
+  {
+    userId: text("user_id")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    wordId: integer("word_id")
+      .notNull()
+      .references(() => words.id, { onDelete: "cascade" }),
+    createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
+  },
+  (t) => ({
+    pk: primaryKey({
+      columns: [t.userId, t.wordId],
+    }),
+  })
+);
+
+export const rolesEnum = pgEnum("role", ["user", "moderator", "admin"]);
+
+export const users = pgTable("user", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name"),
+  email: text("email").notNull(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
+  role: rolesEnum("role").default("user"),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  usersToWords: many(usersToWords),
+}));
+
+export const usersToWordsRelations = relations(usersToWords, ({ one }) => ({
+  user: one(users, {
+    fields: [usersToWords.userId],
+    references: [users.id],
+  }),
+  word: one(words, {
+    fields: [usersToWords.wordId],
+    references: [words.id],
+  }),
+}));
+
+export const accounts = pgTable(
+  "account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount["type"]>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+    createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  })
+);
---
+++ drizzle/migrations/0001_calm_slipstream.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "words" RENAME COLUMN "relatedWords" TO "related_words";--> statement-breakpoint
+ALTER TABLE "words" RENAME COLUMN "relatedPhrases" TO "related_phrases";
