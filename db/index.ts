a1e0075ffac5a86b63f3677ba9b8fd800e8eecbf
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-11-26 20:57:35 +0300

    fix: DB_URL changed to DATABASE_URL

--- db/index.ts
+++ db/index.ts
@@ -4,7 +4,7 @@ import postgres from "postgres";
 import * as schema from "./schema";
 import "dotenv/config";
 
-const sql = postgres(process.env.DB_URL!, { max: 1 });
+const sql = postgres(process.env.DATABASE_URL!, { max: 1 });
 export const db = drizzle(sql, { schema });
 
 await migrate(db, { migrationsFolder: "drizzle/migrations" });
