dd9bcf3adad6c7811ffa2dc0c984f706192397d9
Furkan Cengiz <muhammedcengiz1@gmail.com>
2024-06-14 20:28:34 +0300

    fix: build errors cause by missing props

--- src/_pages/search/SearchResult.tsx
+++ src/_pages/search/SearchResult.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { api } from "../../trpc/server";
 import WordCard from "../../components/customs/WordCard";
 
-export default async function SearchResult({ word }: { word: string }) {
+export default async function SearchResult({ word, locale }: { word: string, locale: "en" | "tr" }) {
   const response = await api.word.getWord(word)
   const isSavedWords = await Promise.all(response.map(async (word) => {
     try {
@@ -14,7 +14,7 @@ export default async function SearchResult({ word }: { word: string }) {
 
   }))
   return response.length > 0 ? (
-    response.map((word) => <WordCard key={word.word_data.word_id} word={word} isSavedWord={isSavedWords.find((value) => value.wordId === word.word_data.word_id)!.isSaved} />)
+    response.map((word) => <WordCard key={word.word_data.word_id} word={word} isSavedWord={isSavedWords.find((value) => value.wordId === word.word_data.word_id)!.isSaved} locale={locale} />)
   ) : (
     <>
       <h1 className="text-center text-fs-3">{word}</h1>
--- src/app/[locale]/saved-words/page.tsx
+++ src/app/[locale]/saved-words/page.tsx
@@ -2,12 +2,16 @@ import WordCard from "@/src/components/customs/WordCard";
 import { api } from "@/src/trpc/server";
 import React from "react";
 
-export default async function SavedWords() {
+export default async function SavedWords({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
   const savedWords = await api.user.getSavedWords();
   return (
     <section className="max-w-5xl mx-auto grid gap-5 mt-5">
       {savedWords?.map((word) => (
-        <WordCard key={word.word_data.word_id} word={word} />
+        <WordCard locale={locale as "en" | "tr"} key={word.word_data.word_id} word={word} />
       ))}
     </section>
   );
