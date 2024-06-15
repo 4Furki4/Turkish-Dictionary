b2c7aa1b659c2baa87ac41f756c49f4af8041796
Furkan Cengiz <muhammedcengiz1@gmail.com>
2024-06-15 04:05:49 +0300

    fix: loading.tsx replaced with suspense fallback tecnique

--- src/app/[locale]/(search)/search/[word]/page.tsx
+++ src/app/[locale]/(search)/search/[word]/page.tsx
@@ -2,6 +2,8 @@ import React, { Suspense } from 'react'
 import { api } from '@/src/trpc/server'
 import WordCard from '@/src/components/customs/WordCard'
 import { db } from '@/db';
+import SearchResult from '@/src/_pages/search/SearchResult';
+import Loading from '../_loading';
 
 export const dynamic = "force-dynamic";
 
@@ -29,31 +31,19 @@ export async function generateStaticParams() {
     return data.map((word) => ({ word: word.name.split(" ").join("-") }))
 }
 
-export default async function SearchResultPage({
+export default function SearchResultPage({
     params: { locale, word }
 }: {
     params: { locale: string, word: string }
 
 }) {
     const formattedWord = decodeURI(word).trim().split("-").join(" ")
-    const response = await api.word.getWord(formattedWord)
-    const isSavedWords = await Promise.all(response.map(async (word) => {
-        try {
-            const isSaved = await api.user.getWordSaveStatus(word.word_data.word_id)
-            return { wordId: word.word_data.word_id, isSaved: isSaved }
-        } catch (error) {
-            return { wordId: word.word_data.word_id, isSaved: false }
-        }
-    }))
-    return <section className="flex flex-col gap-4 px-6 max-w-7xl mx-auto">
-        {response.length > 0 ? (
-            response.map((word) => <WordCard key={word.word_data.word_id} word={word} isSavedWord={isSavedWords.find((value) => value.wordId === word.word_data.word_id)!.isSaved} locale={locale as "en" | "tr"} />)
-        ) : (
-            <>
-                <h1 className="text-center text-fs-3">{formattedWord}</h1>
-                <p className="text-center text-fs-3">Word not found!</p>
-            </>
-        )}
-    </section>
+    return (
+        <section className="flex flex-col gap-4 px-6 max-w-7xl mx-auto">
+            <Suspense fallback={<Loading />}>
+                <SearchResult word={formattedWord} locale={locale as "en" | "tr"} />
+            </Suspense>
+        </section>
+    )
 
 }
