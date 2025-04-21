e5d87289839b1b5c6713baacea03a7084a9171d8
Furkan Cengiz <muhammedcengiz1@gmail.com>
2025-04-21 20:40:35 +0300

    refactor: Sidebar localization

--- src/components/customs/sidebar.tsx
+++ src/components/customs/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client"
-import { HistoryIcon, HomeIcon, LayoutDashboard, ListTree, StarIcon } from 'lucide-react'
+import { HomeIcon, LayoutDashboard, ListTree, StarIcon } from 'lucide-react'
 import React from 'react'
-import { Link as NextIntlLink, usePathname } from "@/src/i18n/routing";
+import { Link as NextIntlLink } from "@/src/i18n/routing";
 import { Session } from 'next-auth';
 import {
     Sheet,
@@ -10,9 +10,7 @@ import {
     SheetTitle,
 } from "@/components/ui/sheet"
 import { Separator } from '@/components/ui/separator';
-import { useTheme } from 'next-themes';
-import { useLocale } from "next-intl";
-import { useParams, useSearchParams } from "next/navigation";
+import { useTranslations } from 'next-intl';
 export default function Sidebar(
     {
         session,
@@ -25,51 +23,44 @@ export default function Sidebar(
             setIsSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>
         }
 ) {
-    const { theme, setTheme } = useTheme();
-    const pathName = usePathname();
-    const searchParams = useSearchParams();
-    const locale = useLocale();
-    const params = useParams();
-    const isAuthPage = ["/signup", "/signin", "/forgot-password"].includes(
-        pathName
-    );
+    const t = useTranslations()
     return (
         <>
             <Sheet open={isSidebarOpen} onOpenChange={(isOpen) => {
                 setIsSidebarOpen(isOpen)
             }}>
-                <SheetContent hideCloseButton={true} side={'left'} className="w-[200px] sm:w-[240px] max-sm:p-0 max-sm:pt-8 sidebar">
+                <SheetContent hideCloseButton={true} side={'left'} className="w-[240px] sm:w-[280px] max-sm:p-0 max-sm:pt-8 sidebar">
                     <SheetHeader>
-                        <SheetTitle>Turkish Dictionary</SheetTitle>
+                        <SheetTitle>{t("Navbar.Title")}</SheetTitle>
                     </SheetHeader>
                     <nav className='p-5'>
                         <ul className='sticky top-4 max-h-max space-y-4'>
                             <Separator />
                             <li>
                                 <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/'}>
-                                    <HomeIcon className="h-6 w-6" /> <span className={`text-nowrap`}>Home</span>
+                                    <HomeIcon className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.Home")}</span>
                                 </NextIntlLink>
                             </li>
                             <li>
                                 <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/word-list'}>
-                                    <ListTree className="h-6 w-6" /> <span className={`text-nowrap`}>Word List</span>
+                                    <ListTree className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.Word List")}</span>
                                 </NextIntlLink>
                             </li>
                             <Separator />
                             <li className='flex'>
                                 <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/saved-words'}>
-                                    <StarIcon className="h-6 w-6" /> <span className={`text-nowrap`}>Saved Words</span>
+                                    <StarIcon className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.SavedWords")}</span>
                                 </NextIntlLink>
                             </li>
-                            <li>
+                            {/* <li>
                                 <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/saved-words'}>
                                     <HistoryIcon className="h-6 w-6" /> <span className={`text-nowrap`}>Search History</span>
                                 </NextIntlLink>
-                            </li>
-                            {session?.user.role === "admin" ? (
+                            </li> */}
+                            {session?.user?.role === "admin" ? (
                                 <li>
                                     <NextIntlLink href={"/dashboard"} className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm'>
-                                        <LayoutDashboard className='h-6 w-6' /> <span className={`text-nowrap`}>Dashboard</span>
+                                        <LayoutDashboard className='h-6 w-6' /> <span className={`text-nowrap`}>{t("Navbar.Dashboard")}</span>
                                     </NextIntlLink>
                                 </li>
                             ) : null}
