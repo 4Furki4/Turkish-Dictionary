482ff1096552949bd9472daf61150e2c9826982b
Furkan Cengiz <muhammedcengiz1@gmail.com>
2025-04-25 02:18:37 +0300

    refactor: middleware matcher to include search and arama routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,5 +20,17 @@ const handleI18nRouting = createMiddleware(routing);
 export default handleI18nRouting
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)',]
-}
+  // Matcher entries are linked with a logical "or", therefore
+  // if one of them matches, the middleware will be invoked.
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    '/((?!api|_next|_vercel|.*\\..*).*)',
+
+    // However, match all pathnames within `/search` or `/arama`, optionally with a locale prefix
+    '/([\\w-]+)?/search/(.+)',
+
+    '/([\\w-]+)?/arama/(.+)'
+  ]
+};
