2f39874be1d07ef9873c69136cc0cc77ec0dd24b
Furkan Cengiz <muhammedcengiz1@gmail.com>
2025-05-17 21:34:55 +0300

    refactor: remove metadata generation for search page to reduce bundle size

--- src/app/[locale]/(search)/search/page.tsx
+++ src/app/[locale]/(search)/search/page.tsx
@@ -1,22 +1,5 @@
-import { api } from "@/src/trpc/server";
 import { redirect } from "next/navigation";
-// if the first searched word is not found, this will reduce bundle size by not importing WordCard component.
-export async function generateMetadata(
-  props: {
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-  }
-) {
-  const searchParams = await props.searchParams;
-  const word = searchParams.word as string;
-  if (word) {
-    const parsedWord = decodeURIComponent(word); // parse the word to utf-8 format string
-    const response = await api.word.getWord({ name: parsedWord, skipLogging: true });
-    return {
-      title: parsedWord,
-      description: response[0]?.word_data.meanings.map((meaning) => meaning.meaning).join(", "),
-    };
-  }
-}
+
 
 export default async function Page(
   props: {
