249936f429ced7774cdba22945423ee0b8beb8ad
Furkan Cengiz <muhammedcengiz1@gmail.com>
2025-07-03 04:27:54 +0300

    ref: add logo, feedback page link, and my requests section to sidebar navigation

--- src/components/customs/sidebar.tsx
+++ src/components/customs/sidebar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { BellIcon, HeartHandshake, HistoryIcon, HomeIcon, LayoutDashboard, ListTree, MicIcon, StarIcon, UserIcon } from 'lucide-react'
+import { BellIcon, GitPullRequestArrow, HandHeart, HeartHandshake, HistoryIcon, HomeIcon, LayoutDashboard, ListTree, LogIn, MicIcon, StarIcon, UserIcon } from 'lucide-react'
 import React from 'react'
 import { Link as NextIntlLink } from "@/src/i18n/routing";
 import { Session } from 'next-auth';
@@ -11,6 +11,8 @@ import {
 } from "@/components/ui/sheet"
 import { Separator } from '@/components/ui/separator';
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+import logo from "@/public/logo.svg";
 export default function Sidebar(
     {
         session,
@@ -31,7 +33,9 @@ export default function Sidebar(
             }}>
                 <SheetContent hideCloseButton={true} side={'left'} className="w-[240px] sm:w-[280px] max-sm:p-0 max-sm:pt-8 sidebar">
                     <SheetHeader>
-                        <SheetTitle>{t("Navbar.Title")}</SheetTitle>
+                        <SheetTitle className='flex items-center gap-2 justify-center text-center text-primary font-bold'>
+                            <Image src={logo} alt="Turkish Dictionary Logo" className="h-6 w-6" />{t("Navbar.Title")}
+                        </SheetTitle>
                     </SheetHeader>
                     <nav className='p-5'>
                         <ul className='sticky top-4 max-h-max space-y-4'>
@@ -57,31 +61,53 @@ export default function Sidebar(
                                 </NextIntlLink>
                             </li>
                             <li>
+                                <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/feedback'}>
+                                    <HandHeart className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.Feedback")}</span>
+                                </NextIntlLink>
+                            </li>
+                            <li>
                                 <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/announcements'}>
                                     <BellIcon className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.Announcements")}</span>
                                 </NextIntlLink>
                             </li>
                             <Separator />
                             {session?.user?.id ? (
-                                <li>
-                                    <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={{
-                                        pathname: "/profile/[id]",
-                                        params: { id: session?.user?.id },
-                                    }}>
-                                        <UserIcon className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.Profile")}</span>
-                                    </NextIntlLink>
-                                </li>
-                            ) : null}
-                            <li className='flex'>
-                                <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/saved-words'}>
-                                    <StarIcon className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.SavedWords")}</span>
-                                </NextIntlLink>
-                            </li>
-                            <li>
-                                <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/search-history'}>
-                                    <HistoryIcon className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.SearchHistory")}</span>
-                                </NextIntlLink>
-                            </li>
+                                <>
+                                    <li>
+                                        <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={{
+                                            pathname: "/profile/[id]",
+                                            params: { id: session?.user?.id },
+                                        }}>
+                                            <UserIcon className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.Profile")}</span>
+                                        </NextIntlLink>
+                                    </li>
+                                    <li className='flex'>
+                                        <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/saved-words'}>
+                                            <StarIcon className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.SavedWords")}</span>
+                                        </NextIntlLink>
+                                    </li>
+                                    <li>
+                                        <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/search-history'}>
+                                            <HistoryIcon className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.SearchHistory")}</span>
+                                        </NextIntlLink>
+                                    </li>
+                                    <li>
+                                        <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/my-requests'}>
+                                            <GitPullRequestArrow className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.MyRequests")}</span>
+                                        </NextIntlLink>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li>
+                                        <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/signin'}>
+                                            <LogIn className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.Sign In")}</span>
+                                        </NextIntlLink>
+                                    </li>
+                                </>
+                            )}
+
+
                             {session?.user?.role === "admin" ? (
                                 <li>
                                     <NextIntlLink href={"/dashboard"} className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm'>
