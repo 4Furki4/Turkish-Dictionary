38717668ce731913b86e10c4fc93514ebf646f0f
Furkan Cengiz <muhammedcengiz1@gmail.com>
2025-07-03 03:55:12 +0300

    ref: authentication check and redirect for pronunciation voting page

--- src/app/[locale]/pronunciation-voting/page.tsx
+++ src/app/[locale]/pronunciation-voting/page.tsx
@@ -1,9 +1,21 @@
 import { api } from "@/src/trpc/server";
 import { HydrateClient } from "@/src/trpc/server";
 import { PronunciationVotingPage } from "@/src/_pages/requests/pronunciation-voting-page";
+import { redirect } from "next/navigation";
+import { auth } from "@/src/server/auth/auth";
 
 export default async function Pronunciations() {
-  await api.request.getVotablePronunciationRequests.prefetch({
+
+  const session = await auth();
+  const callbackUrl = "/pronunciation-voting";
+  const queryParams = new URLSearchParams();
+
+  queryParams.set("callbackUrl", callbackUrl);
+  if (!session) {
+    redirect(`/signin?${queryParams.toString()}`);
+  }
+
+  void api.request.getVotablePronunciationRequests.prefetch({
     page: 1,
     limit: 10,
     search: "",
